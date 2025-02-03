fcbdcb1b08a6feb472d2c276c96c60ddd41c5c6c
Adrian <adrianmroz82@gmail.com>
2025-02-03 20:22:42 +0100

    add login functionality

---
+++ src/components/admin/AdminSidebar.tsx
@@ -0,0 +1,57 @@
+import { Database, PlusCircle, Text } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/shadcn-ui/sidebar";
+// import { ThemeSwitcher } from "@/components/ui/theme-switcher";
+
+const items = [
+  {
+    title: "Zarzadzaj produktami",
+    url: "/admin/products",
+    icon: Database,
+  },
+  {
+    title: "Dodaj nowy produkt",
+    url: "/admin/add-product",
+    icon: PlusCircle,
+  },
+  {
+    title: "Zarzadzaj trescia",
+    url: "/admin/content",
+    icon: Text,
+  },
+];
+
+export function AdminSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild size="lg">
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>{/* <ThemeSwitcher /> */}</SidebarFooter>
+    </Sidebar>
+  );
+}
--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -29,10 +29,10 @@ export async function updateSession(request: NextRequest) {
     data: { user },
   } = await supabase.auth.getUser();
 
-  if (!user && !request.nextUrl.pathname.startsWith("/admin/login") && !request.nextUrl.pathname.startsWith("/auth")) {
+  if (!user && !request.nextUrl.pathname.startsWith("/login") && !request.nextUrl.pathname.startsWith("/auth")) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
-    url.pathname = "/admin/login";
+    url.pathname = "/login";
     return NextResponse.redirect(url);
   }
 
