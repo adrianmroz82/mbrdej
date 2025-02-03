4c33840d96cab536dcebfa44c3d2cc7563094d23
Adrian <adrianmroz82@gmail.com>
2025-02-03 20:08:20 +0100

    add basic products page

---
+++ src/app/(admin)/admin/dashboard/page.tsx
@@ -0,0 +1,17 @@
+import { redirect } from "next/navigation";
+
+import { createClient } from "@/utils/supabase/server";
+import { Button } from "@/components/shadcn-ui/button";
+
+export default async function DashboardPage() {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase.auth.getUser();
+
+  // TODO - should be in middleware/api route?
+  if (error || !data?.user) {
+    redirect("/admin/login");
+  }
+
+  return <Button>Hello {data.user.email}</Button>;
+}
