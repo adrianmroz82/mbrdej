68213fcab59a564e05f6a16298f0e17f685778b4
Adrian <adrianmroz82@gmail.com>
2025-06-17 21:13:24 +0200

    add contact page

--- src/app/(client)/kontakt/page.tsx
+++ src/app/(client)/kontakt/page.tsx
@@ -1,3 +1,36 @@
+// https://supabase.com/docs/guides/functions/examples/send-emails
+
+import ContactForm from "@/components/ui/contact-form";
+
+function HeaderSection({ title, description }: { title: string; description: string }) {
+  return (
+    <div className="text-center mb-8 bg-slate-200 w-full py-8 px-4">
+      <p className="text-4xl font-bold mb-4 text-gray-800">{title}</p>
+      <p className="text-xl italic text-gray-600 max-w-2xl mx-auto">{description}</p>
+    </div>
+  );
+}
+
 export default async function KontaktPage() {
-  return <div>Kontakt</div>;
+  const title = "Kontakt";
+  const description =
+    "Jesteś zainteresowany kupnem, obrazem na zamówienie lub chcesz o coś zapytać? Zapraszam do kontaktu.";
+
+  return (
+    <>
+      <HeaderSection title={title} description={description} />
+
+      <div className="my-6 px-4 max-w-4xl mx-auto">
+        <h2 className="font-bold mb-2">Jak kupować</h2>
+        <p>
+          Jeśli jesteś zainteresowany zakupem danego obrazu lub masz pytania wystarczy, że się ze mną skontaktujesz.
+        </p>
+        <p>Napisz mail, wypełnij formularz lub skontaktuj się ze mną przez Instagram.</p>
+      </div>
+
+      <div className="my-6 px-4 md:w-[50rem] w-full mx-auto">
+        <ContactForm />
+      </div>
+    </>
+  );
 }
