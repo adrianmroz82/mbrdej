e16e75081f40a64f59ecf2238e1b1e70643d0327
Adrian <adrianmroz82@gmail.com>
2025-06-18 09:12:55 +0200

    add homepage content

--- src/page.config.ts
+++ src/page.config.ts
@@ -17,6 +17,15 @@ export const config = {
       href: "/kontakt",
     },
   ],
+  // TODO: move to content table
+  homePage: {
+    title: "OBRAZY OLEJNE, AKWARELA, PLAKATY",
+    description:
+      "Jestem Małgorzata, a to miejsce gdzie dzielę się swoją pasją. Poznaj moją sztukę i zabierz ją do swojego życia...",
+    footerTitle: "INDYWIDUALNE ZAMÓWIENIE",
+    footerDescription:
+      "Chciałbyś mieć obraz o konkretnej tematyce, kolorach czy rozmiarze, według swojego upodobania? A może marzy ci się uwiecznienie konkretnego kadru na płótnie? Chętnie stworzę obraz dla Ciebie, namalowany specjalnie do Twojego wnętrza. Zapraszam do kontaktu.",
+  },
 };
 
 export type PageConfig = typeof config;
