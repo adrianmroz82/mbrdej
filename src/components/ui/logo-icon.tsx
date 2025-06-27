ae642ce904e687227d9755710bf37b331293dc10
Adrian <adrianmroz82@gmail.com>
2025-06-27 10:02:29 +0200

    add logo and contact page

--- src/components/ui/logo-icon.tsx
+++ src/components/ui/logo-icon.tsx
@@ -1,7 +1,6 @@
-// TODO: remove padding on svg
 import Image from "next/image";
 
-import logo from "@/images/pieczatka.png";
+import logo from "@/images/logo-stamp.png";
 
 export function LogoIcon() {
   return <Image src={logo} alt="Logo" width={100} height={100} className="h-24 w-24" />;
