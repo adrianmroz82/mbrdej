25143e428708067bb4fba87ccf5498ef03f6ff62
Adrian <adrianmroz82@gmail.com>
2025-07-19 19:55:06 +0200

    move content to db

--- src/app/(client)/o-mnie/page.tsx
+++ src/app/(client)/o-mnie/page.tsx
@@ -1,4 +1,3 @@
-// TODO: generic function getContent that will fetch data from content table and pass it to component
 import Image from "next/image";
 
 import { HeaderSection } from "@/components/ui/header-section";
@@ -8,26 +7,26 @@ export default async function AboutPage() {
   const supabase = await createClient();
   const { data } = await supabase.from("content-about-me").select().single();
 
+  const { title, description, image_url } = data!;
+
   return (
     <>
-      <HeaderSection title={data?.title!} />
+      <HeaderSection title={title} />
       <div className="md:w-10/12 w-full mx-auto">
         <div className="flex flex-col lg:flex-row ">
           <div className="w-full lg:w-1/2 p-4">
-            <p className="text-lg mt-4 text-balance">{data?.description}</p>
+            <p className="text-lg mt-4 text-balance">{description}</p>
           </div>
 
-          {data?.image_url && (
-            <div className="w-full lg:w-1/2 p-4 mt-4 ">
-              <Image
-                src={data.image_url}
-                alt="O mnie"
-                className="mx-auto overflow-hidden rounded-xl object-cover sm:w-full"
-                width="550"
-                height="550"
-              />
-            </div>
-          )}
+          <div className="w-full lg:w-1/2 p-4 mt-4 ">
+            <Image
+              src={image_url}
+              alt={title}
+              className="mx-auto overflow-hidden rounded-xl object-cover sm:w-full"
+              width="550"
+              height="550"
+            />
+          </div>
         </div>
       </div>
     </>
